2f682754aac17ec95f6b8f371b2030df5bd760fa
CarlosAnguiano <carlos.joseanguiano@gmail.com>
2017-09-05 18:54:12 -0500

    Multimedia onActivityResult 05

--- app/src/main/java/com/bsdenterprise/carlos/anguiano/multimedia/Activity/PhotoViewerActivity.java
+++ app/src/main/java/com/bsdenterprise/carlos/anguiano/multimedia/Activity/PhotoViewerActivity.java
@@ -1,22 +1,32 @@
 package com.bsdenterprise.carlos.anguiano.multimedia.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.bsdenterprise.carlos.anguiano.multimedia.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import static com.bsdenterprise.carlos.anguiano.multimedia.Activity.MainSingleAlbumActivity.EXTRA_RESULT_SELECTED_PICTURE;
 
@@ -35,6 +45,9 @@ public class PhotoViewerActivity extends AppCompatActivity implements ViewPager.
     private String typeFile;
     private EditText photoDescription;
     private Toolbar toolbar;
+    boolean iconRemove = false;
+    private int position;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,6 +66,92 @@ public class PhotoViewerActivity extends AppCompatActivity implements ViewPager.
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK && mImagePaths.size() != 0) {
+
+                try {
+                    viewPager.setAdapter(null);
+                    Uri resultUri = result.getUri();
+                    String newPath = resultUri.getPath();
+                    mImagePaths.remove(position);
+                    mImagePaths.add(newPath);
+//                    adapter = new PhotoViewerAdapter(this, mImagePaths);
+//                    createViewPager();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Log.i(TAG, "onActivityResult: " + result.getError());
+            }
+        }
+        if (requestCode == 20 && resultCode == Activity.RESULT_OK) {
+            viewPager.setAdapter(null);
+            paths = data.getExtras().getStringArrayList(EXTRA_RESULT_SELECTED_PICTURE);
+            if (paths != null) {
+                mImagePaths = paths;
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < mImagePaths.size(); i++) {
+                    stringBuilder.append(mImagePaths.get(i));
+                }
+                String newString = stringBuilder.toString();
+                if (newString.contains(".jpg") || (newString.contains(".png")) || (newString.contains(".jpeg"))) {
+//                    adapter = new PhotoViewerAdapter(this, paths);
+                    createViewPager();
+                }
+            }
+        }
+    }
+
+    public void createViewPager() {
+        FrameLayout frameLayoutI = (FrameLayout) findViewById(R.id.fragmentContainer);
+        viewPager = new ViewPager(this);
+        CoordinatorLayout.LayoutParams params = new CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.MATCH_PARENT, CoordinatorLayout.LayoutParams.MATCH_PARENT);
+        viewPager.setId(R.id.viewPagerNew);
+        viewPager.setLayoutParams(params);
+        frameLayoutI.addView(viewPager);
+//        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.clear();
+        MenuItem item = menu.add(Menu.NONE, R.id.crop_image_menu_crop, Menu.NONE, R.string.crop_image_menu_crop);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        item.setIcon(R.drawable.crop);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.crop_image_menu_crop:
+//                final int[] chatMessage = new int[1];
+////                chatMessage[0] = adapter.getItemPosition(currentPage);
+////                adapter.removeItem(currentPage);
+//                position = viewPager.getCurrentItem();
+//
+//                String a = adapter.getCurrentItem(position);
+//                CropImage.activity(Uri.fromFile(new File(a)))
+//                        .setGuidelines(CropImageView.Guidelines.ON)
+//                        .start(this);
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
